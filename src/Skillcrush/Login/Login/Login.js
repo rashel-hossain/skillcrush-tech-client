4e2c121a72837170e2d75f6ead7ba80a845e1185
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-10-25 17:16:09 +0600

    login UI form done

--- src/Skillcrush/Login/Login/Login.js
+++ src/Skillcrush/Login/Login/Login.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
         <div>
-            <h2>This is Login</h2>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col">
+                    <div className="text-center mb-5">
+                        <h1 className="text-5xl font-bold">Login now!</h1>
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <div className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="text" placeholder="email" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="text" placeholder="password" className="input input-bordered" />
+
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Login</button>
+                            </div>
+
+                            <p><small>Already have an account?
+                                <Link to='/register' className="label-text-alt link link-hover underline"> Register now</Link>
+                            </small></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
