ec048f237e520bf1edf9acaa1ce2871ba8c5ff02
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-10-25 17:43:51 +0600

    register UI form done

--- src/Skillcrush/Login/Login/Login.js
+++ src/Skillcrush/Login/Login/Login.js
@@ -5,9 +5,9 @@ const Login = () => {
     return (
         <div>
             <div className="hero min-h-screen bg-base-200">
-                <div className="hero-content flex-col">
+                <div className="hero-content flex-col w-1/2">
                     <div className="text-center mb-5">
-                        <h1 className="text-5xl font-bold">Login now!</h1>
+                        <h1 className="text-4xl font-bold">Login now!</h1>
                     </div>
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                         <div className="card-body">
@@ -17,22 +17,25 @@ const Login = () => {
                                 </label>
                                 <input type="text" placeholder="email" className="input input-bordered" />
                             </div>
+
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="text" placeholder="password" className="input input-bordered" />
-
+                                <input type="password" placeholder="password" className="input input-bordered" />
                             </div>
+
                             <div className="form-control mt-6">
                                 <button className="btn btn-primary">Login</button>
                             </div>
-
-                            <p><small>Already have an account?
+                            <p><small>Don't have an account?
                                 <Link to='/register' className="label-text-alt link link-hover underline"> Register now</Link>
                             </small></p>
                         </div>
                     </div>
+                    <span>----------------------or----------------------</span>
+                    <button className="btn btn-outline btn-primary w-full">Continue with Google</button>
+                    <button className="btn btn-outline btn-primary w-full">Continue with GitHub</button>
                 </div>
             </div>
         </div>
--- src/Skillcrush/Login/Register/Register.js
+++ src/Skillcrush/Login/Register/Register.js
@@ -1,9 +1,58 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
         <div>
-            <h2>This is register</h2>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col w-1/2">
+                    <div className="text-center mb-5">
+                        <h1 className="text-4xl font-bold">Register now!</h1>
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <div className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Full Name</span>
+                                </label>
+                                <input type="text" placeholder="your name" className="input input-bordered" />
+                            </div>
+
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Your Photo</span>
+                                </label>
+                                <input type="text" placeholder="your photo url" className="input input-bordered" />
+                            </div>
+
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="text" placeholder="your email" className="input input-bordered" />
+                            </div>
+
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" placeholder="your password" className="input input-bordered" />
+
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Login</button>
+                            </div>
+
+                            <p><small>Already have an account?
+                                <Link to='/login' className="label-text-alt link link-hover underline"> Login now</Link>
+                            </small></p>
+                        </div>
+                    </div>
+                    <span>----------------------or----------------------</span>
+                    <button className="btn btn-outline btn-primary w-full">Continue with Google</button>
+                    <button className="btn btn-outline btn-primary w-full">Continue with GitHub</button>
+                </div>
+            </div>
         </div>
     );
 };
