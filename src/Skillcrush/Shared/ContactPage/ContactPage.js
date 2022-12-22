c0a9b65ac60fa3830a5b04ba9fcaf2c008fb8f2c
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-12-22 11:46:39 +0600

    added contact form design

--- src/Skillcrush/Shared/ContactPage/ContactPage.js
+++ src/Skillcrush/Shared/ContactPage/ContactPage.js
@@ -36,6 +36,72 @@ const ContactPage = () => {
                                     </svg>
                                 </a>
                             </div>
+                            <div className="w-full max-w-xl xl:px-8 xl:w-5/12">
+                                <div className="bg-white rounded shadow-2xl p-7 sm:p-10">
+                                    <h3 className="mb-4 text-xl font-semibold sm:text-center sm:mb-6 sm:text-2xl">
+                                        Contact For Course Info
+                                    </h3>
+                                    <form>
+                                        <div className="mb-1 sm:mb-2">
+                                            <label
+                                                htmlFor="firstName"
+                                                className="inline-block mb-1 font-medium"
+                                            >
+                                                Full Name
+                                            </label>
+                                            <input
+                                                placeholder="Your Name"
+                                                required
+                                                type="text"
+                                                className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                                                id="firstName"
+                                                name="firstName"
+                                            />
+                                        </div>
+                                        <div className="mb-1 sm:mb-2">
+                                            <label
+                                                htmlFor="lastName"
+                                                className="inline-block mb-1 font-medium"
+                                            >
+                                                E-mail
+                                            </label>
+                                            <input
+                                                placeholder="Your E-mail"
+                                                required
+                                                type="text"
+                                                className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                                                id="lastName"
+                                                name="lastName"
+                                            />
+                                        </div>
+                                        <div className="mb-1 sm:mb-2">
+                                            <label
+                                                htmlFor="email"
+                                                className="inline-block mb-1 font-medium"
+                                            >
+                                                Message Us
+                                            </label>
+                                            <textarea
+                                                className='w-full border border-gray-300 rounded shadow-sm'
+                                                placeholder="Your message about our course..."
+                                                required
+                                                type="text"
+                                            ></textarea>
+                                        </div>
+                                        <div className="mt-4 mb-2 sm:mb-4">
+                                            <button
+                                                type="submit"
+                                                className="inline-flex items-center justify-center w-full h-12 px-6 font-medium bg-red-500 text-white font-medium"
+                                            >
+                                                Subscribe
+                                            </button>
+                                        </div>
+                                        <p className="text-xs text-gray-600 sm:text-sm">
+                                            We respect your privacy. Unsubscribe at any time.
+                                        </p>
+                                    </form>
+                                </div>
+                            </div>
 
                         </div>
                     </div>
